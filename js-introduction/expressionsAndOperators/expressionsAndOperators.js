0edfde7d23077df94ea7520e03152e2e74481a1b
Frank <1850780453@qq.com>
2020-07-10 15:44:05 +0800

    [ADD]增加语法和表达式

---
+++ js-introduction/expressionsAndOperators/expressionsAndOperators.js
@@ -0,0 +1,43 @@
+//原始表达式
+//常量，直接量：3.14，"test"
+//关键字：null,this,true
+//变量：i,k,j
+
+//复合表达式
+//10*20（加运算符）
+
+//数组，对象的初始化表达式
+//[1,2]  new Array(1,2);
+//[1,,,4] [1,undefined,undefined,4]
+//{x:1,y:2}  var o = new Object(); o.x = 1;o.y=2;
+
+//函数表达式
+// var fe = function(){}
+// (function(){console.log('hello world')})
+
+//属性访问表达式
+/*var o = {x:1};
+o.x
+0['x']*/
+
+//调用表发生
+// func()
+
+// 对象创建表达式
+// new Func(1,2)
+// new Object;
+
+
+
+
+//运算符
+//一元 +num
+//二元 a+b
+//三元 c?a:b
+//赋值 x+=1
+//比较 a==b
+//算术 a-b
+//位   a|b
+//逻辑 exp1 && exp2
+//字符串 "a" + "b"
+//特殊 delete obj.x
